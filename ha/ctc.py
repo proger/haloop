c9a0eddacb27e82ac4924c920d7153e85fa562bc
Volodymyr Kyrylov <vol@wilab.org.ua>
2023-03-12 23:52:25 +0200

    ctc: get rid of data dependent control flow for vmap

--- ha/ctc.py
+++ ha/ctc.py
@@ -16,12 +16,11 @@ def ctc_forward_score1(
 
     # A B C -> _ A _ B _ C _
     _t_a_r_g_e_t_s_ = torch.stack([torch.full_like(targets, blank), targets], dim=0).mT.reshape(-1)
-    _t_a_r_g_e_t_s_ = torch.cat([_t_a_r_g_e_t_s_, targets.new(1).fill_(blank)], dim=-1)
+    _t_a_r_g_e_t_s_ = torch.cat([_t_a_r_g_e_t_s_, targets.new_full((1,), blank)], dim=-1)
 
     log_alpha = emissions.new_full((T, len(_t_a_r_g_e_t_s_)), float('-inf'))
 
-    log_alpha[0, 0] = emissions[0, _t_a_r_g_e_t_s_[0]]
-    log_alpha[0, 1] = emissions[0, _t_a_r_g_e_t_s_[1]]
+    log_alpha[0, :2] = emissions[0, _t_a_r_g_e_t_s_[:2]]
 
     for t in range(1, T):
         for s in range(1, len(_t_a_r_g_e_t_s_)):
@@ -29,11 +28,21 @@ def ctc_forward_score1(
             prev_symbol = log_alpha[t-1, s-1]
             skip = log_alpha[t-1, s-2]
 
-            if _t_a_r_g_e_t_s_[s] == blank or _t_a_r_g_e_t_s_[s-2] == _t_a_r_g_e_t_s_[s]:
-                # transitions into blank or a copy of the same symbol
-                transitions = self_loop.logaddexp(prev_symbol)
-            else:
-                transitions = self_loop.logaddexp(prev_symbol).logaddexp(skip)
+            base_transitions = self_loop.logaddexp(prev_symbol)
+            transitions_with_skip = base_transitions.logaddexp(skip)
+
+            # transition into blank: no skips across blanks
+            transitions = torch.where(
+                _t_a_r_g_e_t_s_[s] == blank,
+                base_transitions,
+                transitions_with_skip
+            )
+            # transition from the same symbol: must go through a blank (no skips)
+            transitions = torch.where(
+                _t_a_r_g_e_t_s_[s-2] == _t_a_r_g_e_t_s_[s],
+                base_transitions,
+                transitions
+            )
 
             log_alpha[t, s] = transitions + emissions[t, _t_a_r_g_e_t_s_[s]]
 
