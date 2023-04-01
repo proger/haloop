867a92e2c85a41e63156eae1facf346b1e819b99
Volodymyr Kyrylov <vol@wilab.org.ua>
2023-04-01 15:20:03 +0300

    introduce rnnlm

---
+++ ha/rnnlm.py
@@ -0,0 +1,226 @@
+from pathlib import Path
+import argparse
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import wandb
+
+from .symbol_tape import Vocabulary, tokenize_bytes, tokenize_chars, SymbolTape
+
+
+class LM(nn.Module):
+    def __init__(self, vocab_size, emb_dim, hidden_dim, num_layers, dropout=0.0):
+        super().__init__()
+
+        self.num_classes = vocab_size
+        self.hidden_dim = hidden_dim
+        self.num_layers = num_layers
+
+        self.embedding = nn.Embedding(vocab_size, emb_dim)
+        self.rnn = nn.LSTM(emb_dim, hidden_dim, num_layers, dropout=dropout)
+        self.out_layer = nn.Linear(hidden_dim, vocab_size)
+
+        self.out_layer.weight = self.embedding.weight
+
+    def forward(self, input, state):
+        emb = self.embedding(input)
+        output, state = self.rnn(emb, state)
+        output = self.out_layer(output)
+        output = output.view(-1, self.num_classes)
+        return output, state
+
+    def init_hidden(self, batch_size):
+        weight = self.out_layer.weight
+        h = weight.new_zeros(self.num_layers, batch_size, self.hidden_dim)
+        c = weight.new_zeros(self.num_layers, batch_size, self.hidden_dim)
+        return (h,c)
+
+    def truncate_hidden(sellf, state):
+        h,c = state
+        return (h.detach(), c.detach())
+
+
+
+class System:
+    def __init__(self, args):
+        if args.init:
+            checkpoint = torch.load(args.init)
+            self.vocab = checkpoint['vocab']
+        else:
+            self.vocab = None
+
+        if args.train:
+            if args.bytes_as_tokens:
+                self.data, self.vocab = tokenize_bytes(args.train, self.vocab)
+            else:
+                self.data, self.vocab = tokenize_chars(args.train, self.vocab)
+            self.batches = SymbolTape(self.data, args.batch_size, args.bptt_len, pad_id=0)
+
+        vocab_size = len(self.vocab.id_to_string)
+
+        self.model = LM(vocab_size=vocab_size,
+                        emb_dim=args.rnn_size,
+                        hidden_dim=args.rnn_size,
+                        num_layers=args.num_layers,
+                        dropout=args.dropout)
+        self.model = self.model.to(args.device)
+
+        if args.init:
+            self.model.load_state_dict(checkpoint['model'])
+
+        self.optimizer = torch.optim.Adam(params=self.model.parameters(), lr=args.lr)
+        if args.init:
+            self.optimizer.load_state_dict(checkpoint['optimizer'])
+
+        self.loss = nn.CrossEntropyLoss(ignore_index=0)
+
+        self.report_every = 1000
+        self.prompts = [
+            "a"
+        ]
+
+    def state_dict(self):
+        return {
+            'vocab': self.data.vocab,
+            'model': self.model.state_dict(),
+            'optimizer': self.optimizer.state_dict()
+        }
+
+    def prepare_prompt(self, prompt):
+        device = next(self.model.parameters()).device
+
+        prompt_list = [self.vocab.string_to_id[char] for char in prompt]
+        x = torch.tensor(prompt_list).to(device).unsqueeze(1).long()
+
+        return x, self.model.init_hidden(1)
+
+    @torch.inference_mode()
+    def complete(self, prompt, steps=512, sample=False):
+        model = self.model
+        model.eval()
+
+        joiner = ''
+        def cast(s):
+            if isinstance(s, int):
+                nonlocal joiner
+                joiner = b''
+                return s.to_bytes(1, 'big')
+            return s
+
+        out_list = []
+        x, states = self.prepare_prompt(prompt)
+
+        logits, states = model(x, states)
+        prompt_logits = nn.functional.cross_entropy(logits[:-1], x[1:].view(-1), reduction='none').sum() / len(x[1:])
+        probs = F.softmax(logits, dim=-1)
+        probs = probs[-1]
+
+        if sample:
+            ix = probs.multinomial(num_samples=1)
+        else:
+            max_p, ix = torch.topk(probs, k=1, dim=-1)
+
+        out_list.append(cast(self.vocab.id_to_string[int(ix)]))
+        x = ix.unsqueeze(1)
+
+        # decode
+        for k in range(steps):
+            logits, states = model(x, states)
+            probs = F.softmax(logits, dim=-1)
+            if sample:
+                ix = probs.multinomial(1)
+            else:
+                _, ix = torch.topk(probs, k=1, dim=-1)
+            out_list.append(cast(self.vocab.id_to_string[int(ix)]))
+            x = ix
+        return prompt_logits, joiner.join(out_list)
+
+    def train_one_epoch(self):
+        model, batches = self.model, self.batches
+        optimizer, loss_fn = self.optimizer, self.loss
+
+        state = model.init_hidden(args.batch_size)
+        model.train()
+
+        for step in range(len(batches)):
+            batch = batches[step].to(args.device).long()
+            model.train()
+            optimizer.zero_grad()
+            state = model.truncate_hidden(state)
+
+            input = batch[:-1]
+            target = batch[1:].reshape(-1)
+
+            batch_size = input.shape[1]
+            prev_batch_size = state[0].shape[1]
+            if batch_size != prev_batch_size:
+                h,c = state
+                state = h[:, :batch_size, :], c[:, :batch_size, :]
+            output, state = model(input, state)
+            loss = loss_fn(output, target)
+            loss.backward()
+            grad_norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1)
+            optimizer.step()
+
+            if step % self.report_every == 0:
+                print(f"epoch {epoch} step {step}/{len(batches)} train loss: {loss.item():.3f} ppl: {loss.exp().item():.3f} grad_norm: {grad_norm.item():.3f}")
+                for prompt in self.prompts:
+                    _, generated_text = self.complete(prompt, 128, sample=False)
+                    print('greedy', prompt + generated_text)
+                for prompt in self.prompts:
+                    _, generated_text = self.complete(prompt, 128, sample=True)
+                    print('sample', prompt + generated_text)
+                wandb.log({'train/loss': loss.item(),
+                        'train/ppl': loss.exp().item(),
+                        'train/lr': args.lr,
+                        'train/epoch': epoch,
+                        'train/grad_norm': grad_norm.item()})
+            else:
+                wandb.log({'train/loss': loss.item(),
+                        'train/ppl': loss.exp().item(),
+                        #'train/lr': scheduler.get_last_lr()[0],
+                        'train/lr': args.lr,
+                        'train/epoch': epoch,
+                        'train/grad_norm': grad_norm.item()})
+
+
+if __name__ == '__main__':
+    class Formatter(argparse.ArgumentDefaultsHelpFormatter,
+                    argparse.MetavarTypeHelpFormatter):
+        pass
+
+    parser = argparse.ArgumentParser(formatter_class=Formatter)
+    #parser = argparse.ArgumentParser()
+    parser.add_argument('--init', type=Path, help="Path to checkpoint to initialize from")
+    parser.add_argument('--save', type=Path, default='rnnlm.pt', help="Path to save checkpoint to")
+    parser.add_argument('--device', type=str, default='cpu', help='device')
+    parser.add_argument('--lr', default=0.0002, type=float, help='Adam learning rate')
+    parser.add_argument('--dropout', default=0, type=float, help='dropout rate')
+    parser.add_argument('--epochs', default=1, type=int, help='number of training set iterations')
+    parser.add_argument('--batch-size', default=128, type=int, help='batch size')
+    parser.add_argument('--bptt-len', default=256, type=int, help='RNN window size')
+    parser.add_argument('--rnn-size', default=512, type=int, help='RNN width')
+    parser.add_argument('--num-layers', default=1, type=int, help='RNN depth')
+    parser.add_argument('--bytes-as-tokens', action='store_true', help='use bytes as tokens')
+    parser.add_argument('--train', type=Path, help='path to training data')
+    parser.add_argument('--complete', type=str, help='complete this prompt')
+    args = parser.parse_args()
+
+    print(args)
+
+    torch.manual_seed(3407)
+
+    self = System(args)
+
+    if args.train:
+        wandb.init(project='rnnlm', config=args)
+
+        for epoch in range(args.epochs):
+            self.train_one_epoch()
+            torch.save(self.state_dict(), args.save)
+
+    if args.complete:
+        self.model.eval()
+        prompt_score, completion = self.complete("\n" + args.complete, 128, sample=False)
+        print(prompt_score.item(), args.complete + completion)
---
+++ ha/symbol_tape.py
@@ -0,0 +1,106 @@
+import torch
+from pathlib import Path
+
+
+class Vocabulary:
+    def __init__(self, pad_token="<pad>", unk_token='<unk>'):
+        self.id_to_string = {}
+        self.string_to_id = {}
+
+        # add the default pad token
+        self.id_to_string[0] = pad_token
+        self.string_to_id[pad_token] = 0
+
+        # add the default unknown token
+        self.id_to_string[1] = unk_token
+        self.string_to_id[unk_token] = 1
+
+        # shortcut access
+        self.pad_id = 0
+        self.unk_id = 1
+
+    def __len__(self):
+        return len(self.id_to_string)
+
+    def add_new_word(self, string):
+        self.string_to_id[string] = len(self.string_to_id)
+        self.id_to_string[len(self.id_to_string)] = string
+
+    # Given a string, return ID
+    def get_idx(self, string, extend_vocab=False):
+        if string in self.string_to_id:
+            return self.string_to_id[string]
+        elif extend_vocab:  # add the new word
+            self.add_new_word(string)
+            return self.string_to_id[string]
+        else:
+            return self.unk_id
+
+
+def tokenize_bytes(text_file, vocab, extend_vocab=True, device='cpu'):
+    if vocab is None:
+        vocab = Vocabulary(pad_token=0, unk_token=7) # nul and bel
+
+    full_text = []
+    print(f"Reading binary file from: {text_file}")
+    with open(text_file, 'rb') as bin:
+        for line in bin:
+            for byte in line:
+                full_text.append(vocab.get_idx(byte, extend_vocab=extend_vocab))
+
+    data = torch.tensor(full_text, device=device, dtype=torch.int16)
+    return data, vocab
+
+
+def tokenize_chars(text_file, vocab, extend_vocab=True, device='cpu'):
+    if vocab is None:
+        vocab = Vocabulary()
+
+    full_text = []
+    print(f"Reading text file from: {text_file}")
+    with open(text_file, 'r') as text:
+        for line in text:
+            tokens = list(line)
+            for token in tokens:
+                full_text.append(vocab.get_idx(token, extend_vocab=extend_vocab))
+
+    data = torch.tensor(full_text, device=device, dtype=torch.int16)
+    return data, vocab
+
+
+
+class SymbolTape:
+    def __init__(self, data, batch_size, bptt_len, pad_id):
+        self.batches = self.create_batch(data, batch_size, bptt_len, pad_id)
+
+    def __len__(self):
+        return len(self.batches)
+
+    def __getitem__(self, idx):
+        return self.batches[idx]
+
+    def create_batch(self, input_data, batch_size, bptt_len, pad_id):
+        batches = []  # each element in `batches` is (len, B) tensor
+        text_len = len(input_data)
+        segment_len = text_len // batch_size + 1
+
+        padded = input_data.data.new_full((segment_len * batch_size,), pad_id)
+        padded[:text_len] = input_data.data
+        padded = padded.view(batch_size, segment_len).t()
+        num_batches = segment_len // bptt_len + 1
+
+        for i in range(num_batches):
+            # Prepare batches such that the last symbol of the current batch
+            # is the first symbol of the next batch.
+            if i == 0:
+                # Append a dummy start symbol using pad token
+                batch = torch.cat(
+                    [padded.new_full((1, batch_size), pad_id),
+                     padded[i * bptt_len:(i + 1) * bptt_len]], dim=0)
+                batches.append(batch)
+            else:
+                batches.append(padded[i * bptt_len - 1:(i + 1) * bptt_len])
+
+        return batches
+
+
