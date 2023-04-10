488ffd5fa0bfd6e707e5d1d6cfe73a8734718365
Volodymyr Kyrylov <vol@wilab.org.ua>
2023-04-10 15:08:58 +0300

    transducer_forward_score1: precompute all joints in one go, unroll recursions a little bit

--- ha/transducer.py
+++ ha/transducer.py
@@ -3,42 +3,41 @@ import torch
 
 @torch.jit.script
 def transducer_forward_score1(
-    transcription_probabilities, # (T, K)  # f   # time starts at 0
-    prediction_probabilities, # (U, K)     # g   # first symbol is blank (0)
-    targets # (U,)                         # y   # first symbol is blank (0)
+    transcription_probs, # (T, K)  # f   # time starts at 0
+    prediction_probs, # (U, K)     # g   # first symbol is blank (0)
+    targets # (U,)                 # y   # first symbol is blank (0)
 ):
     """Transducer forward score for a single sequence (using probabilities).
 
     [Graves12] Sequence Transduction with Recurrent Neural Networks
     """
-    T, K = transcription_probabilities.shape
-    U, K = prediction_probabilities.shape
+    T, K = transcription_probs.shape
+    U, K = prediction_probs.shape
 
-    alpha = transcription_probabilities.new_zeros((T+1, U))
-    y_prob = transcription_probabilities.new_zeros((T, U))
-    blank_prob = transcription_probabilities.new_zeros((T+1, U))
+    joint_probs = (transcription_probs[:, None, :] + prediction_probs[None, :, :]).softmax(dim=-1)
 
-    alpha[-1, 0] = 1
-    blank_prob[-1, 0] = 1
+    alpha = transcription_probs.new_zeros((T, U))
+    t = 0
+    alpha[t, 0] = 1
+    for u in range(1, U):
+        prev_symbol_prob = joint_probs[t, u-1, targets[u-1]]
+        alpha[t, u] = alpha[t-1, u].clone() * 1. + alpha[t, u-1].clone() * prev_symbol_prob
 
-    #print(alpha.T)
+    #print(t, alpha.T)
 
-    for t in range(T):
-        f = transcription_probabilities[t]
+    for t in range(1, T):
+        u = 0
+        prev_blank_prob = joint_probs[t-1, u, 0]
+        alpha[t, u] = alpha[t-1, u].clone() * prev_blank_prob
 
-        for u in range(U):
-            g = prediction_probabilities[u]
+        for u in range(1, U):
+            prev_blank_prob  = joint_probs[t-1, u, 0]
+            prev_symbol_prob = joint_probs[t, u-1, targets[u-1]]
+            alpha[t, u] = alpha[t-1, u].clone() * prev_blank_prob + alpha[t, u-1].clone() * prev_symbol_prob
 
-            h = (f + g).softmax(dim=-1)
+        #print(t, alpha.T)
 
-            blank_prob[t, u] = h[0]
-            y_prob[t, u] = h[targets[u]]
-
-            alpha[t, u] = alpha[t-1, u].clone() * blank_prob[t-1, u].clone() + alpha[t, u-1].clone() * y_prob[t, u-1].clone()
-
-        #print(alpha.T)
-
-    return alpha[T-1, U-1] * y_prob[T-1, U-1]
+    return alpha[T-1, U-1] * joint_probs[T-1, U-1, 0]
 
 
 if __name__ == '__main__':
