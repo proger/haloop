1c133acbe77c9dc27a9a89d97180282595fd82ed
Volodymyr Kyrylov <vol@wilab.org.ua>
2023-05-05 10:17:26 +0300

    training loop for a transducer starts working with hac --lm

--- ha/loop.py
+++ ha/loop.py
@@ -16,7 +16,9 @@ from .model import Encoder, CTCRecognizer, StarRecognizer
 from .resnet import FixupResNet, FixupBasicBlock
 from .xen import Vocabulary
 from . import symbol_tape
-
+from .rnnlm import LM
+from .transducer import transducer_forward_score
+from .ctc import ctc_reduce_mean
 
 console = Console()
 def print(*args, flush=False, **kwargs):
@@ -62,15 +64,35 @@ class System(nn.Module):
                 self.recognizer = StarRecognizer(star_penalty=star_penalty,
                                                  vocab_size=len(self.vocab)).to(args.device)    
 
-        self.optimizer = torch.optim.Adam(chain(self.encoder.parameters(), self.recognizer.parameters()), lr=args.lr)
+        if args.lm:
+            lm_checkpoint = torch.load(args.lm, map_location=args.device)
+            self.lm_args = lm_checkpoint['args']
+            self.lm = LM(vocab_size=len(self.vocab),
+                         emb_dim=self.lm_args['rnn_size'],
+                         hidden_dim=self.lm_args['rnn_size'],
+                         num_layers=self.lm_args['num_layers'],
+                         dropout=self.lm_args['dropout']).to(args.device)
+            #self.lm.load_state_dict(lm_checkpoint['model'])
+
+            self.optimizer = torch.optim.Adam(chain(self.encoder.parameters(),
+                                                    self.recognizer.parameters(),
+                                                    self.lm.parameters()), lr=args.lr)
+        else:
+            self.lm_args, self.lm = None, None
+
+            self.optimizer = torch.optim.Adam(chain(self.encoder.parameters(),
+                                                    self.recognizer.parameters()), lr=args.lr)
         self.scaler = torch.cuda.amp.GradScaler()
 
     def load_state_dict(self, checkpoint):
         self.encoder.load_state_dict(checkpoint['encoder'])
         self.recognizer.load_state_dict(checkpoint['recognizer'])
         self.scaler.load_state_dict(checkpoint['scaler'])
-        self.optimizer.load_state_dict(checkpoint['optimizer'])
+        #self.optimizer.load_state_dict(checkpoint['optimizer'])
         self.vocab.load_state_dict(checkpoint['vocab'])
+        if self.lm is not None:
+            print('loading transducer lm')
+            self.lm.load_state_dict(checkpoint['lm'])
 
     def make_state_dict(self, **extra):
         return {
@@ -79,10 +101,60 @@ class System(nn.Module):
             'scaler': self.scaler.state_dict(),
             'optimizer': self.optimizer.state_dict(),
             'vocab': self.vocab.state_dict(),
+            'lm_args': self.lm_args,
+            'lm': self.lm.state_dict() if self.lm is not None else None,
         } | extra
 
+    def forward(self, inputs, targets, input_lengths, target_lengths):
+        device = self.args.device
+
+        N, _, _ = inputs.shape
+        inputs = inputs.to(device) # (N, T, C)
+        targets = targets.to(device) # (N, U)
+        input_lengths = input_lengths.to(device) # (N,)
+        target_lengths = target_lengths.to(device) # (N,)
+
+        #print(inputs, targets) # works best with --batch-size 1
+
+        input_lengths = self.encoder.subsampled_lengths(input_lengths)
+
+        with torch.autocast(device_type='cuda', dtype=torch.float16):
+            outputs = self.encoder(inputs) # (N1, T, C)
+
+            if self.lm is not None:
+                #
+                # Output dependencies are controller by LM
+                #
+                hidden = self.lm.init_hidden(N)
+
+                # input needs to start with 0
+                lm_targets = torch.cat([targets.new_zeros((N, 1)), targets], dim=1) # (N, U1)
+
+                lm_outputs, _ = self.lm.forward_batch_first(lm_targets, hidden) # (N, U1, C)
+
+                outputs = self.recognizer.dropout(outputs)
+                outputs = self.recognizer.classifier(outputs) # (N, T, C)
+
+                joint = outputs[:, :, None, :] + lm_outputs[:, None, :, :] # (N, T, U1, C)
+                #joint = joint.log_softmax(dim=-1)
+
+                #loss = ctc_reduce_mean(transducer_forward_score(joint, targets, input_lengths, target_lengths), target_lengths)
+
+                from torchaudio.functional import rnnt_loss
+                loss = rnnt_loss(joint,
+                                    targets.to(torch.int32),
+                                    input_lengths.to(torch.int32),
+                                    target_lengths.to(torch.int32),
+                                    blank=0, reduction='mean', fused_log_softmax=True)
+            else:
+                #
+                # All outputs are independent
+                #
+                loss = self.recognizer(outputs, targets, input_lengths, target_lengths)
+
+        return loss, outputs
+
     def train_one_epoch(self, epoch, train_loader):
-        args, device = self.args, self.args.device
         encoder, recognizer, optimizer, scaler = self.encoder, self.recognizer, self.optimizer, self.scaler
 
         optimizer.zero_grad()
@@ -92,18 +164,7 @@ class System(nn.Module):
         train_loss = 0.
         t0 = time.time()
         for i, (inputs, targets, input_lengths, target_lengths) in enumerate(train_loader):
-            inputs = inputs.to(device)
-            targets = targets.to(device)
-            input_lengths = input_lengths.to(device)
-            target_lengths = target_lengths.to(device)
-
-            #print(inputs, targets) # works best with --batch-size 1
-
-            input_lengths = encoder.subsampled_lengths(input_lengths)
-
-            with torch.autocast(device_type='cuda', dtype=torch.float16):
-                outputs = encoder(inputs)
-                loss = recognizer(outputs, targets, input_lengths, target_lengths)
+            loss, _ = self.forward(inputs, targets, input_lengths, target_lengths)
 
             if torch.isnan(loss):
                 print(f'[{epoch + 1}, {i + 1:5d}], loss is nan, skipping batch', flush=True)
@@ -116,7 +177,9 @@ class System(nn.Module):
 
             scaler.scale(loss).backward()
             scaler.unscale_(optimizer)
-            grad_norm = torch.nn.utils.clip_grad_norm_(chain(encoder.parameters(), recognizer.parameters()), 0.1)
+            grad_norm = torch.nn.utils.clip_grad_norm_(chain(encoder.parameters(),
+                                                             recognizer.parameters(),
+                                                             self.lm.parameters()), 0.1)
             if torch.isinf(grad_norm) or torch.isnan(grad_norm):
                 print(f'[{epoch + 1}, {i + 1:5d}], grad_norm is inf or nan, skipping batch', flush=True)
                 scaler.update()
@@ -128,16 +191,16 @@ class System(nn.Module):
             optimizer.zero_grad(set_to_none=True)
 
             train_loss += loss.item()
-            if i and i % args.log_interval == 0:
-                train_loss = train_loss / args.log_interval
+            if i and i % self.args.log_interval == 0:
+                train_loss = train_loss / self.args.log_interval
                 t1 = time.time()
                 print(f'[{epoch + 1}, {i + 1:5d}] time: {t1-t0:.3f} loss: {train_loss:.3f} grad_norm: {grad_norm:.3f}', flush=True)
                 wandb.log({'train/loss': train_loss, 'train/grad_norm': grad_norm})
                 t0 = t1
+                train_loss = 0.
 
     @torch.inference_mode()
     def evaluate(self, epoch, valid_loader):
-        device = self.args.device
         encoder, recognizer, vocabulary = self.encoder, self.recognizer, self.vocab
 
         valid_loss = 0.
@@ -146,20 +209,11 @@ class System(nn.Module):
         encoder.eval()
         recognizer.eval()
         for i, (inputs, targets, input_lengths, target_lengths) in enumerate(valid_loader):
-            inputs = inputs.to(device)
-            targets = targets.to(device)
-            input_lengths = input_lengths.to(device)
-            target_lengths = target_lengths.to(device)
-
-            input_lengths = encoder.subsampled_lengths(input_lengths)
-
-            outputs = encoder(inputs)
-            loss = recognizer(outputs, targets, input_lengths, target_lengths)
+            loss, outputs = self.forward(inputs, targets, input_lengths, target_lengths)
 
             valid_loss += loss.item()
 
             if i < 10:
-                outputs = recognizer.log_probs(outputs)
                 for ref, ref_len, seq, hyp_len in zip(targets, target_lengths, outputs, input_lengths):
                     seq = seq[:hyp_len].cpu()
                     ref = ref[:ref_len].cpu().tolist()
@@ -220,6 +274,7 @@ def make_parser():
     parser.add_argument('--star-penalty', type=float, default=None, help="Star penalty for Star CTC. If None, train with regular CTC")
     parser.add_argument('--num-workers', type=int, default=32, help="Number of workers for data loading")
     parser.add_argument('--vocab', type=str, default='bytes', choices=['bytes', 'cmu', 'xen'], help="Vocabulary to use: raw bytes, CMUdict, Xen (CMUdict + glottal closures)")
+    parser.add_argument('--lm', type=Path, help="Path to language model checkpoint trained with hal.")
     return parser
 
 
