ece167784773ea6f72c7a2b39ae6828a94ef6326
Volodymyr Kyrylov <vol@wilab.org.ua>
2023-08-31 00:18:12 +0300

    active_loop: add oracle-max-wer query strategy

--- ha/active_loop.py
+++ ha/active_loop.py
@@ -3,6 +3,8 @@ from pathlib import Path
 import pandas as pd
 import sys
 from ha.subprocess import run
+import numpy as np
+from kaldialign import edit_distance
 
 parser = argparse.ArgumentParser(description="""Active learning on noisy labels.
 
@@ -37,7 +39,7 @@ parser.add_argument('--exp', type=Path, default=Path('exp/active/egl/01'),
 parser.add_argument('--vocab', type=Path, default=Path('data/corrupted-librispeech/libribpe.vocab'),
                     help='vocab file')
 parser.add_argument('--device', type=str, default='cuda', help='device')
-
+parser.add_argument('--strategy', type=str, choices=['egl', 'oracle-max-wer'], default='egl', help='query strategy')
 
 def clean_tokens(text):
     return ' '.join([token for token in text.split() if token != '␣'])
@@ -62,6 +64,7 @@ def read_grads(filename: Path):
             rows.append((int(dataset_index), float(grad_norm), float(loss)))
     return pd.DataFrame(rows, columns=['dataset_index', 'grad_norm', 'loss']).set_index('dataset_index')
 
+
 def training_log_to_dataset(training_log_filename: Path):
     "reads output of hac using heuristics to extract the dataset"
     train_hypotheses = []
@@ -82,6 +85,32 @@ def training_log_to_dataset(training_log_filename: Path):
     return df.set_index('dataset_index')
 
 
+def estimate_egl(
+    grad_norms_df: pd.DataFrame # ['grad_norm', 'loss', 'media_filename']
+) -> pd.Series:
+    #from scipy.special import logsumexp
+    # #
+    # #    \log \sum_y ||\grad P(y|x)||**2 P(y|x)
+    # # =  \log \sum_y exp(\log ||\grad P(y|x)||**2 - NLL(y|x))
+    # #
+    # grad_norms['product'] = np.log((grad_norms['grad_norm'] ** 2)) - grad_norms['loss']
+    # egl = grad_norms.groupby('media_filename')['product'].apply(logsumexp)
+
+    grad_norms_df['product'] = (grad_norms_df['grad_norm'] ** 2) * np.exp(-grad_norms_df['loss'])
+    egl = grad_norms_df.groupby('media_filename')['product'].apply(np.sum)
+
+    egl.sort_values(ascending=False, inplace=True)
+    return egl
+
+
+def compute_wer_pointwise(hyp_df, ref_df):
+    joint_df = hyp_df.merge(ref_df, on='media_filename', suffixes=('_hyp', '_ref'))
+    edits = joint_df.apply(lambda x: edit_distance(x['text_hyp'], x['text_ref']), axis=1, result_type='expand')
+    joint_df = joint_df.join(edits)
+    lengths = pd.DataFrame(joint_df.apply(lambda x: {'length': len(x['text_ref'].split())}, axis=1, result_type='expand'))
+    return joint_df.join(lengths)
+
+
 if __name__ == '__main__':
     args = parser.parse_args()
 
@@ -100,68 +129,60 @@ if __name__ == '__main__':
 
     args.exp.mkdir(exist_ok=True, parents=True)
 
-    if not (args.exp / 'last.pt').exists() or not (args.exp / 'train.log').exists():
-        prefixes = ['mask:fbank:speed:', 'mask:fbank:speed:randpairs:']
-        run([
-            'hac',
-            '--train', ','.join([prefix + str(combined_train) for prefix in prefixes]),
-            '--eval', 'fbank:data/corrupted-librispeech/dev-clean.txt.piece',
-            '--test-attempts', '20',
-            '--test', f'fbank:{corrupted}'
-            ] + f'--num-epochs 13 --num-workers 16 --lr_decay_iters 15835 --lr_schedule linear --warmup_iters 3000 --batch-size 48 --lr 0.0006 --min_lr 0 --eval-batch-size 1024 --compile --vocab {str(args.vocab)} --weight_decay 0.1'.split() + [
-            '--exp', f'{args.exp}', '--allow-oom',
-            '--device', args.device,
-        ], output_filename=args.exp / 'train.log')
-        just_trained = True
-    else:
-        just_trained = False
+    match args.strategy:
+        case 'oracle-max-wer':
+            wer_df = compute_wer_pointwise(prev_corrupted_dataset, oracle)
+            query = wer_df.sort_values('total', ascending=False).head(args.query_size)
+            query = query.set_index('media_filename')
+        case 'egl':
+            if not (args.exp / 'last.pt').exists() or not (args.exp / 'train.log').exists():
+                prefixes = ['mask:fbank:speed:', 'mask:fbank:speed:randpairs:']
+                run([
+                    'hac',
+                    '--train', ','.join([prefix + str(combined_train) for prefix in prefixes]),
+                    '--eval', 'fbank:data/corrupted-librispeech/dev-clean.txt.piece',
+                    '--test-attempts', '20',
+                    '--test', f'fbank:{corrupted}'
+                    ] + f'--num-epochs 13 --num-workers 16 --lr_decay_iters 15835 --lr_schedule linear --warmup_iters 3000 --batch-size 48 --lr 0.0006 --min_lr 0 --eval-batch-size 1024 --compile --vocab {str(args.vocab)} --weight_decay 0.1'.split() + [
+                    '--exp', f'{args.exp}', '--allow-oom',
+                    '--device', args.device,
+                ], output_filename=args.exp / 'train.log')
+                just_trained = True
+            else:
+                just_trained = False
 
-    train_hypotheses = training_log_to_dataset(args.exp / 'train.log')
-    grad_norms_dataset = train_hypotheses.join(prev_corrupted_dataset)
+            train_hypotheses = training_log_to_dataset(args.exp / 'train.log')
+            grad_norms_dataset = train_hypotheses.join(prev_corrupted_dataset)
 
-    if not (args.exp / 'grads.txt').exists() or just_trained:
-        print('# writing', args.exp / 'hyp.txt.piece', file=sys.stderr)
-        grad_norms_dataset[['media_filename', 'hyp_text']].to_csv(args.exp / 'hyp.txt.piece', sep='\t', header=False, index=False)
-        print('# computing gradient norms', file=sys.stderr)
-        run([
-            'hac',
-            '--grad-norms', f'fbank:{args.exp / "hyp.txt.piece"}',
-            '--device', args.device,
-            '--init', str(args.exp / 'last.pt'),
-            '--vocab', str(args.vocab),
-            '--compile',
-        ], output_filename=args.exp / 'grads.txt')
-    else:
-        print('# using existing', args.exp / 'grads.txt', file=sys.stderr)
-        run(["wc", "-l", args.exp / 'grads.txt'])
+            if not (args.exp / 'grads.txt').exists() or just_trained:
+                print('# writing', args.exp / 'hyp.txt.piece', file=sys.stderr)
+                grad_norms_dataset[['media_filename', 'hyp_text']].to_csv(args.exp / 'hyp.txt.piece', sep='\t', header=False, index=False)
+                print('# computing gradient norms', file=sys.stderr)
+                run([
+                    'hac',
+                    '--grad-norms', f'fbank:{args.exp / "hyp.txt.piece"}',
+                    '--device', args.device,
+                    '--init', str(args.exp / 'last.pt'),
+                    '--vocab', str(args.vocab),
+                    '--compile',
+                ], output_filename=args.exp / 'grads.txt')
+            else:
+                print('# using existing', args.exp / 'grads.txt', file=sys.stderr)
+                run(["wc", "-l", args.exp / 'grads.txt'])
 
-    grad_norms_result = read_grads(args.exp / 'grads.txt')
+            grad_norms_result = read_grads(args.exp / 'grads.txt')
 
-    # Compute EGL for each utterance
-    grad_norms = pd.concat([
-        grad_norms_dataset.reset_index(),
-        grad_norms_result
-    ], axis=1)
+            # Compute EGL for each utterance
+            grad_norms_df = pd.concat([
+                grad_norms_dataset.reset_index(),
+                grad_norms_result
+            ], axis=1)
 
-    import numpy as np
+            egl = estimate_egl(grad_norms_df)
+            egl.to_csv(args.exp / 'egl', sep='\t', header=False)
+            print('# writing utterance scores to', args.exp / 'egl', file=sys.stderr)
 
-    #from scipy.special import logsumexp
-    # #
-    # #    \log \sum_y ||\grad P(y|x)||**2 P(y|x)
-    # # =  \log \sum_y exp(\log ||\grad P(y|x)||**2 - NLL(y|x))
-    # #
-    # grad_norms['product'] = np.log((grad_norms['grad_norm'] ** 2)) - grad_norms['loss']
-    # egl = grad_norms.groupby('media_filename')['product'].apply(logsumexp)
-
-    grad_norms['product'] = (grad_norms['grad_norm'] ** 2) * np.exp(-grad_norms['loss'])
-    egl = grad_norms.groupby('media_filename')['product'].apply(np.sum)
-
-    egl.sort_values(ascending=False, inplace=True)
-
-    egl.to_csv(args.exp / 'egl', sep='\t', header=False)
-    print('# writing utterance scores to', args.exp / 'egl', file=sys.stderr)
-
-    query = egl[:args.query_size]
+            query = egl[:args.query_size]
 
     print('# querying', len(query), 'clean utterances')
 
